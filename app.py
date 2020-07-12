84886322a8f579394c8bdb7d35aaba51c3047607
Arthur <artuhha1@gmail.com>
2020-07-12 20:14:16 +0300

    Messages handling

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import schedule, time, multiprocessing
+import schedule, time, concurrent.futures
 from datetime import datetime
 from vk_bot import VkBot
 # group_token = '***REMOVED***'
 
 
 def make_post():
-    local_stats, global_stats = vk.get_info()
+    local_stats, global_stats = vk.get_stats(get_global=True, get_local=True)
     current_date = datetime.now().strftime('%d.%m.%Y')
     message = (
         f"Статистика по коронавирусу за {current_date}\n"
@@ -26,6 +26,51 @@ def make_post():
     )
     vk.wall_post(message)
 
+def messages_hander():
+    messages = {
+        'ru': ['рф', "ру", "россия", "в россии", 'ru', 'russia'],
+        'world': ['мир', "в мире", 'world', 'worldwide'],
+        'all': ['коронавирус', "коронавирус статистика", "covid", "все", "статистика"]
+    }
+    while True:
+        event = vk.messages_loop()
+        msg = event.message.text.lower().strip('[club***REMOVED***|@covid_stats] ')
+        peer_id = event.message.peer_id
+        out_msg = ""
+        if msg in messages['ru'] or msg in messages['all']:
+            stats = vk.get_stats(get_local=True)[0]
+            out_msg += (
+                f"\nВ России:\n"
+                f"Новых случаев: {stats['total_new_cases_today']}\n"
+                f"Смертей сегодня: {stats['total_new_deaths_today']}\n"
+                f"Всего случаев: {stats['total_cases']}\n"
+                f"Из них выздоровело: {stats['total_recovered']}\n"
+                f"Всего смертей: {stats['total_deaths']}\n"
+                f"Уровень опасности: {stats['total_danger_rank']}\n"
+            )
+        if msg in messages['world'] or msg in messages['all']:
+            stats = vk.get_stats(get_global=True)[0]
+            out_msg += (
+                "\nВ мире:\n"
+                f"Новых случаев: {stats['total_new_cases_today']}\n"
+                f"Смертей сегодня: {stats['total_new_deaths_today']}\n"
+                f"Всего случаев: {stats['total_cases']}\n"
+                f"Из них выздоровело: {stats['total_recovered']}\n"
+                f"Всего смертей: {stats['total_deaths']}\n"
+                f"Зараженных стран: {stats['total_affected_countries']}"
+            )
+        if msg == 'начать':
+            out_msg = (
+                'Привет! Я бот - коронавирус. Я могу сообщать тебе последнюю статистику по коронавирусу.\n\n'
+                'Чтобы начать, просто напиши одну из трех комманд: \n'
+                'ру - Узнать статистику в России.\n'
+                'мир - Узнать о ситуации в мире.\n'
+                'все - Статистика России и мира в одном сообщении.\n'
+            )
+        if out_msg != '': vk.send_message(peer_id, out_msg)
+
+
+
 def schedule_loop():
     while True:
         time.sleep(20)
@@ -33,6 +78,9 @@ def schedule_loop():
 
 
 if __name__ == "__main__":
-    vk = VkBot('***REMOVED***', ***REMOVED***)
-    schedule.every().day.at('19:00').do(make_post)
-    schedule_loop()
+    vk = VkBot('***REMOVED***', '***REMOVED***', ***REMOVED***)
+    schedule.every().day.at('16:00').do(make_post)
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        t1 = executor.submit(schedule_loop)
+        t2 = executor.submit(messages_hander)
--- vk_bot/vk_bot.py
+++ vk_bot/vk_bot.py
@@ -1,28 +1,34 @@
 from vk_api import VkApi
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import json, requests
 
+
 class VkBot:
-    def __init__(self, token: str, group_id: int):
+    def __init__(self, token: str, group_token: str, group_id: int):
         self.group_id = group_id
         vk_session = VkApi(token = token, config_filename = '.vk_config.v2.json')
         self.vk = vk_session.get_api()
+        self.group_session = VkApi(token=group_token, config_filename = '.vk_config.v2.json')
+        self.group_vk = self.group_session.get_api()
 
-    def get_info(self):
-        local_stats = json.loads(requests.get('https://api.thevirustracker.com/free-api?countryTotal=RU').text)['countrydata'][0]
-        global_stats = json.loads(requests.get('https://api.thevirustracker.com/free-api?global=stats').text)['results'][0]
-        return local_stats, global_stats
+    def get_stats(self, get_local = False, get_global = False) -> dict:
+        out = []
+        if get_local: out.append(json.loads(requests.get('https://api.thevirustracker.com/free-api?countryTotal=RU').text)['countrydata'][0])
+        if get_global: out.append(json.loads(requests.get('https://api.thevirustracker.com/free-api?global=stats').text)['results'][0])
+        return out
 
     def wall_post(self, message):
         post_id = self.vk.wall.post(owner_id = -self.group_id, message = message, from_group = True)['post_id']
         self.vk.wall.pin(owner_id = -self.group_id, post_id = post_id)
+
+    def send_message(self, peer_id, message):
+        self.group_vk.messages.send(peer_id = peer_id, message = message, random_id = 0)
     
     def messages_loop(self):
-        session = VkApi(token='***REMOVED***')
-        longpoll = VkLongPoll(session)
+        longpoll = VkBotLongPoll(self.group_session, self.group_id)
         for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW:
-                print(event.message)
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                return event
 
 
 if __name__ == "__main__":
