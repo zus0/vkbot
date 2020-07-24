b55094f835e2930a211f06c1984cae44f1d2be2a
Arthur <artuhha1@gmail.com>
2020-07-25 01:49:03 +0300

    Advanced logging

--- app.py
+++ app.py
@@ -1,83 +1,123 @@
-import schedule
-import time
-import yaml
-import re
+import getopt
 import logging
+import re
+import sys
+import time
+import schedule
+import yaml
 from concurrent import futures
-from datetime import datetime
 from vk_bot import VkBot, Api as api
 
-def initialize_logging(filename):
-    open(filename, 'w') # clean the file
-    logging.basicConfig(filename=filename, level = logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
-    logging.info('Started!')
+class VK:
+    def __init__(self, data_, log_level):
+        log = logging.getLogger('bot')
+        log.setLevel(log_level)
+        fmt = logging.Formatter('[%(asctime)s] %(levelname)s: %(name)s: %(message)s')
 
-def make_post():
-    try:
-        local, world = api.get_stats(local = True, world = True)
-        local_yesterday, global_yesterday = api.get_stats(local = True, world = True, yesterday = True)
-        current_date = datetime.fromtimestamp(world['updated']/1000).strftime('%d.%m.%Y %H:%M')
-        message = f'Статистика по коронавирусу на {current_date}\nПишите в лс боту, чтобы получить последнюю статистику!\n\n'
-        message += api.arrange_message(local, local_yesterday, 'В России:')
-        message += api.arrange_message(world, global_yesterday, 'В мире:')
-        vk.wall_post(message)
-    except TypeError:
-        logging.warning('Got TypeError in make_post()')
-        time.sleep(900)
-        make_post()
+        fh = logging.FileHandler('bot.log', 'w')
+        fh.setFormatter(fmt)
+        log.addHandler(fh)
 
-def messages_hander():
-    messages = {
-        'ru': ['рф', "ру", "россия", 'ru', 'russia'],
-        'world': ['мир', "в мире", 'world', 'worldwide'],
-        'all': ['коронавирус', "covid", "все", "статистика"],
-        'start': ['начать', "помощь"]
-    }
-    while True:
+        ch = logging.StreamHandler()
+        ch.setFormatter(fmt)
+        ch.setLevel(logging.WARNING)
+        log.addHandler(ch)
+
+        self.group_id = data_['group_id']
+        self.vk = VkBot(data_['user_token'], data_['group_token'], data_['group_id'])
+        self.log = log
+
+
+    def make_post(self):
         try:
-            event = vk.get_event()
-            msg = re.sub(fr'\[{data["group_id"]}|.*\](,|) ', '', event.message['text'].lower()) # remove @ from message
-            msg = msg.encode('cp1251', 'ignore').decode('cp1251').strip(' ') # remove all emojis and strip the space
-            peer_id = event.message.peer_id
-            out_msg = ""
-            if msg in messages['ru'] or msg in messages['all']:
-                stats = api.get_stats(local = True)[0]
-                stats_yesterday = api.get_stats(local = True, yesterday = True)[0]
-                out_msg += api.arrange_message(stats, stats_yesterday, 'В России:')
-            if msg in messages['world'] or msg in messages['all']:
-                stats = api.get_stats(world = True)[0]
-                stats_yesterday = api.get_stats(world = True, yesterday = True)[0]
-                out_msg += api.arrange_message(stats, stats_yesterday, 'В мире:')
-            if msg in messages['start'] or event.from_chat and msg == '':
-                out_msg = (
-                    'Привет! Я бот - коронавирус. Я могу сообщать тебе последнюю статистику по коронавирусу.\n\n'
-                    'Чтобы начать, просто напиши одну из трех комманд: \n'
-                    'ру - Узнать статистику в России.\n'
-                    'мир - Узнать о ситуации в мире.\n'
-                    'все - Статистика России и мира в одном сообщении.\n'
-                )
-            if out_msg != '': vk.send_message(peer_id, out_msg)
-            else: vk.send_message(peer_id, 'Извини, но я тебя не понимаю. Напиши "помощь", чтобы получить список команд.')
-        except TypeError:
-            logging.warning('Got TypeError in messages_handler()')
-            vk.send_message(peer_id, 'Произошла ошибка. Повторите попытку позднее.')
+            local, world = api.get_stats(local = True, world = True)
+            local_yesterday, global_yesterday = api.get_stats(local = True, world = True, yesterday = True)
+
+            message = 'Пишите в лс боту, чтобы получить последнюю статистику!\n'
+            message += api.arrange_message(local, local_yesterday, 'в России')
+            message += api.arrange_message(world, global_yesterday, 'в мире')
+
+            self.vk.wall_post(message)
+        except:
+            self.log.exception('')
 
 
+    def messages_hander(self):
+        messages = {
+            'ru': ['рф', "ру", "россия", 'ru', 'russia'],
+            'world': ['мир', "в мире", 'world', 'worldwide'],
+            'all': ['коронавирус', "covid", "все", "статистика"],
+            'start': ['начать', "помощь"]
+        }
 
-def schedule_loop():
-    while True:
-        logging.debug("Schedule loop")
-        schedule.run_pending()
-        time.sleep(60)
+        while True:
+            try:
+                event = self.vk.get_event()
+                self.log.info('Got event with type %s', event.type)
+                msg = re.sub(fr'\[{self.group_id}|.*\](,|) ', '', event.message['text'].lower()) # remove @ from message
+                msg = msg.encode('cp1251', 'ignore').decode('cp1251').strip(' ') # remove all emojis and strip the space
+                peer_id = event.message.peer_id
+                self.log.info('Event message is %s, peer id is %s', msg, peer_id)
+                out_msg = ""
+
+                if msg in messages['ru'] or msg in messages['all']:
+                    stats = api.get_stats(local = True)[0]
+                    stats_yesterday = api.get_stats(local = True, yesterday = True)[0]
+                    out_msg += api.arrange_message(stats, stats_yesterday, 'в России')
+
+                if msg in messages['world'] or msg in messages['all']:
+                    stats = api.get_stats(world = True)[1]
+                    stats_yesterday = api.get_stats(world = True, yesterday = True)[1]
+                    out_msg += api.arrange_message(stats, stats_yesterday, 'в мире')
+
+                if msg in messages['start'] or event.from_chat and msg == '':
+                    out_msg = (
+                        'Привет! Я бот - коронавирус. Я могу сообщать тебе последнюю статистику по коронавирусу.\n\n'
+                        'Чтобы начать, просто напиши одну из трех комманд: \n'
+                        'ру - Узнать статистику в России.\n'
+                        'мир - Узнать о ситуации в мире.\n'
+                        'все - Статистика России и мира в одном сообщении.\n'
+                    )
+
+                if out_msg != '': self.vk.send_message(peer_id, out_msg)
+                else: self.vk.send_message(peer_id, 'Извини, но я тебя не понимаю. Напиши "помощь", чтобы получить список команд.')
+            except TypeError:
+                self.log.warning('Got TypeError in messages_handler()')
+                self.vk.send_message(peer_id, 'Произошла ошибка. Повторите попытку позднее.')
+            except:
+                self.log.exception('')
+                raise
+
+
+    def schedule_loop(self):
+        while True:
+            try:
+                self.log.debug("Schedule loop")
+                schedule.run_pending()
+                time.sleep(60)
+            except:
+                self.log.exception('')
+                raise
+
+
+def get_arguments():
+    options = getopt.getopt(sys.argv[1:], "", ["log="])[0]
+    numeric_level = 30
+    out = {'numeric_level': numeric_level}
+    for opt, arg in options:
+        if opt == '--log':
+            out['numeric_level'] = getattr(logging, arg.upper(), numeric_level)
+    return out
 
 
 if __name__ == "__main__":
-    initialize_logging('bot.log')
     with open('data.yaml', 'r') as f:
         data = yaml.load(f, Loader = yaml.FullLoader)
-    vk = VkBot(data['user_token'], data['group_token'], data['group_id'])
-    schedule.every().day.at('16:00').do(make_post)
-    
+
+    args = get_arguments()
+    vk = VK(data, args['numeric_level'])
+    schedule.every().day.at('16:00').do(vk.make_post)
+
     with futures.ThreadPoolExecutor() as executor:
-        t1 = executor.submit(schedule_loop)
-        t2 = executor.submit(messages_hander)
+        t1 = executor.submit(vk.schedule_loop)
+        t2 = executor.submit(vk.messages_hander)
--- vk_bot/vk_bot.py
+++ vk_bot/vk_bot.py
@@ -1,61 +1,76 @@
+import json
+import logging
+import requests
+from datetime import datetime
 from vk_api import VkApi, keyboard
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import json
-import requests
-import logging
 
 class VkBot:
     def __init__(self, user_token: str, group_token: str, group_id: int):
-        self.group_id = group_id
         vk_session = VkApi(token = user_token, config_filename = '.vk_config.v2.json')
+        group_session = VkApi(token = group_token, config_filename = '.vk_config.v2.json')
+
+        self.group_id = group_id
         self.vk = vk_session.get_api()
-        self.group_session = VkApi(token = group_token, config_filename = '.vk_config.v2.json')
-        self.group_vk = self.group_session.get_api()
-        self.longpoll = VkBotLongPoll(self.group_session, self.group_id)
+        self.group_vk = group_session.get_api()
+        self.longpoll = VkBotLongPoll(group_session, self.group_id)
+        self.log = logging.getLogger('vk.vk_bot')
+
         self.kb = keyboard.VkKeyboard()
         for option in ["Россия 🇷🇺", "Мир 🌎", "Все 📊"]: self.kb.add_button(option, color='primary')
 
     def wall_post(self, message):
         post_id = self.vk.wall.post(owner_id = -self.group_id, message = message, from_group = True)['post_id']
         self.vk.wall.pin(owner_id = -self.group_id, post_id = post_id)
-        logging.info("Wall post with id of %s", post_id)
+        self.log.info("Wall post with id of %s", post_id)
 
     def send_message(self, peer_id, message):
         kb = (self.kb.get_keyboard() if peer_id < 2000000000 else None)
-        self.group_vk.messages.send(peer_id = peer_id, message = message, random_id = 0, keyboard = kb)
+        message_id = self.group_vk.messages.send(peer_id = peer_id, message = message, random_id = 0, keyboard = kb)
+        self.log.info("Sent message with id %s", message_id)
     
     def get_event(self):
         while True:
             try:
                 for event in self.longpoll.listen():
-                    logging.debug("Got event with type %s", event.type)
+                    self.log.debug("Got event with type %s", event.type)
                     if event.type == VkBotEventType.MESSAGE_NEW:
                         return event
             except requests.ReadTimeout:
-                logging.warning('Request timeout in longpoll.listen()')
+                self.log.warning('Request timeout in longpoll.listen()')
+            except requests.exceptions.ConnectionError:
+                self.log.warning('Connection Error in longpoll.listen()')
+            except:
+                self.log.exception('Exception in longpoll.listen()')
 
 class Api:
-    @staticmethod
-    def get_stats(local = False, world = False, yesterday = False) -> list:
-        out = []
+    log = logging.getLogger('vk.Api')
+
+    @classmethod
+    def get_stats(cls, local = False, world = False, yesterday = False) -> dict:
+        out = [[], []]
         yesterday = ('true' if yesterday else 'false')
         try:
-            if local: out.append(json.loads(requests.get('https://disease.sh/v3/covid-19/countries/Russia', params={"yesterday": yesterday}).text))
-            if world: out.append(json.loads(requests.get('https://disease.sh/v3/covid-19/all', params={"yesterday": yesterday}).text))
+            if local: out[0] = json.loads(requests.get('https://disease.sh/v3/covid-19/countries/Russia', params={"yesterday": yesterday}).text)
+            if world: out[1] = json.loads(requests.get('https://disease.sh/v3/covid-19/all', params={"yesterday": yesterday}).text)
         except json.JSONDecodeError:
-            logging.warning('Unable to decode API data')
-        logging.debug(out)
+            cls.log.warning('Unable to decode API data')
+        cls.log.debug(out)
         return out
 
-    @staticmethod
-    def arrange_message(stats, stats_yesterday, header) -> str:
-        out = f'\n{header}\n'
+    @classmethod
+    def arrange_message(cls, stats, stats_yesterday, header) -> str:
+        deaths_percent = round(stats['deaths']/(stats['cases']/100), 2)
+        recovered_percent = round(stats['recovered']/(stats['cases']/100), 2)
+        current_date = datetime.fromtimestamp(stats['updated']/1000).strftime('%d.%m.%Y %H:%M')
+
         cases_diff = "{:+}".format(stats['todayCases'] - stats_yesterday['todayCases'])
         deaths_diff = "{:+}".format(stats['todayDeaths'] - stats_yesterday['todayDeaths'])
         recovered_diff = "{:+}".format(stats['todayRecovered'] - stats_yesterday['todayRecovered'])
         active_diff = "{:+}".format(stats['active'] - stats_yesterday['active'])
-        recovered_percent = round(stats['recovered']/(stats['cases']/100), 2)
-        deaths_percent = round(stats['deaths']/(stats['cases']/100), 2)
+
+        out = f'\nСтатистика {header} на {current_date}:\n'
+
         if stats['todayCases'] == 0:
             out += 'На сегодня нет новой информации.\n'
         else:
@@ -64,13 +79,15 @@ class Api:
                 f"Смертей сегодня: {stats['todayDeaths']} ({deaths_diff})\n"
                 f"Сегодня выздоровело: {stats['todayRecovered']} ({recovered_diff})\n"
             )
+
         out += (
             f"Всего случаев: {stats['cases']}\n"
             f"Из них выздоровело: {stats['recovered']} ({recovered_percent}%)\n"
             f"Всего смертей: {stats['deaths']} ({deaths_percent}%)\n"
             f"Активных случаев: {stats['active']} ({active_diff})\n"
         )
-        logging.debug(out)
+
+        cls.log.debug(out)
         return out
 
 
